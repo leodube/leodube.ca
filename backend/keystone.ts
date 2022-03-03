3e5accc00b32ffc05ba4dd0f712db6f6c482a1db
Leo Dube <leo.s.dube@gmail.com>
2022-03-03 10:11:55 -0800

    Add graphql and db debugging in development mode

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -2,6 +2,7 @@ import { config } from "@keystone-6/core";
 import { statelessSessions } from "@keystone-6/core/session";
 import { lists } from "./src/schema";
 import { withAuth } from "./src/auth";
+import type { GraphQLConfig } from "@keystone-6/core/types";
 import {
   PORT,
   DATABASE_URL,
@@ -16,15 +17,24 @@ const session = statelessSessions({
 });
 
 // Configure Keystone
-export default withAuth(
-  config({
+export default config(
+  withAuth({
     db: {
       provider: "postgresql",
       useMigrations: true,
+      enableLogging: false,
       url: DATABASE_URL,
     },
     server: { port: PORT },
     lists,
     session,
+    ui: {
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+    graphql: {
+      debug: process.env.NODE_ENV !== "production",
+      queryLimits: { maxTotalResults: 100 },
+      path: "/api/graphql",
+    },
   })
 );
